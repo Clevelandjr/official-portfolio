58bc896899f9df536d71e2ffc85e39604ff01a6c
Cleveland Pitt <cleveland.pitt@gmail.com>
2020-08-01 11:58:30 -0400

    used codepen to get my code the way i want it to look, but now receiving typescript errors

--- portfolio/src/components/Navigation.js
+++ portfolio/src/components/Navigation.js
@@ -1,34 +1,88 @@
-Import React from 'react';
-Import {Nav, INavLink, INavStyles, INavLinkGroup} from 'office-ui-fabric-react/lib/Nav';
+import React from 'react';
+import {Nav, INavLink, INavStyles, INavLinkGroup} from 'office-ui-fabric-react/lib/Nav';
 
 // Initialize icons in case this example uses them
 initializeIcons();
 
-const navLinkGroups: INavLinkGroup[] = [
-    {
-      links: [
-        {
-          name: 'Home',
-          url: 'http://example.com',
-          expandAriaLabel: 'Expand Home section',
-          collapseAriaLabel: 'Collapse Home section',
-          links: [
-            {
-              name: 'Activity',
-              url: 'http://msn.com',
-              key: 'key1',
-              target: '_blank',
-            },
-            {
-              name: 'MSN',
-              url: 'http://msn.com',
-              disabled: true,
-              key: 'key2',
-              target: '_blank',
-            },
-          ],
-          isExpanded: true,
-        },
-    }
-    ];
+const navStyles: Partial<INavStyles> = {
+  root: {
+    width: 208,
+    height: 350,
+    boxSizing: 'border-box',
+    border: '1px solid #eee',
+    overflowY: 'auto',
+  },
+};
 
+const navLinkGroups: INavLinkGroup[] = [
+  {
+    links: [
+      {
+        name: 'Home',
+        url: 'http://example.com',
+        key: 'key1',
+        isExpanded: true,
+        target: '_blank',
+      },
+      {
+        name: 'About',
+        url: 'http://example.com',
+        key: 'key2',
+        isExpanded: true,
+        target: '_blank',
+      },
+      {
+        name: 'Resume',
+        url: 'http://example.com',
+        key: 'key3',
+        isExpanded: true,
+        target: '_blank',
+      },
+      {
+        name: 'Web Applications',
+        url: 'http://example.com',
+        expandAriaLabel: 'Expand Home section',
+        collapseAriaLabel: 'Collapse Home section',
+        links: [
+          {
+            name: 'Stats',
+            url: 'http://msn.com',
+            key: 'key4',
+            target: '_blank',
+          },
+          {
+            name: 'MSN',
+            url: 'http://msn.com',
+            disabled: true,
+            key: 'key5',
+            target: '_blank',
+          },
+        ],
+        isExpanded: true,
+      
+      
+      },
+    ],
+  },
+];
+
+const NavBasicExample: React.FunctionComponent = () => {
+  return (
+    <Nav
+      onLinkClick={_onLinkClick}
+      selectedKey="key3"
+      ariaLabel="Nav basic example"
+      styles={navStyles}
+      groups={navLinkGroups}
+    />
+  );
+};
+
+function _onLinkClick(ev?: React.MouseEvent<HTMLElement>, item?: INavLink) {
+  if (item && item.name === 'News') {
+    alert('News link clicked');
+  }
+}
+
+const NavBasicExampleWrapper = () => <Fabric><NavBasicExample /></Fabric>;
+ReactDOM.render(<NavBasicExampleWrapper />, document.getElementById('content'))
