33a39223b4079bf276a7d2bf9993a551b3d11f15
Cleveland Pitt <cleveland.pitt@gmail.com>
2020-08-01 16:22:07 -0400

    redid imports and base code for navigation.js

--- portfolio/src/components/Navigation.js
+++ portfolio/src/components/Navigation.js
@@ -1,88 +1,14 @@
 import React from 'react';
-import {Nav, INavLink, INavStyles, INavLinkGroup} from 'office-ui-fabric-react/lib/Nav';
+import content from '../content/navigation.json';
+import { Nav } from 'office-ui-fabric-react/lib/Nav';
+import { initializeIcons } from 'office-ui-fabric-react/lib/Icons';
 
 // Initialize icons in case this example uses them
 initializeIcons();
 
-const navStyles = {
-  root: {
-    width: 208,
-    height: 350,
-    boxSizing: 'border-box',
-    border: '1px solid #eee',
-    overflowY: 'auto',
-  },
-};
+class Navigation extends React.Component {
 
-const navLinkGroups = [
-  {
-    links: [
-      {
-        name: 'Home',
-        url: 'http://example.com',
-        key: 'key1',
-        isExpanded: true,
-        target: '_blank',
-      },
-      {
-        name: 'About',
-        url: 'http://example.com',
-        key: 'key2',
-        isExpanded: true,
-        target: '_blank',
-      },
-      {
-        name: 'Resume',
-        url: 'http://example.com',
-        key: 'key3',
-        isExpanded: true,
-        target: '_blank',
-      },
-      {
-        name: 'Web Applications',
-        url: 'http://example.com',
-        expandAriaLabel: 'Expand Home section',
-        collapseAriaLabel: 'Collapse Home section',
-        links: [
-          {
-            name: 'Stats',
-            url: 'http://msn.com',
-            key: 'key4',
-            target: '_blank',
-          },
-          {
-            name: 'MSN',
-            url: 'http://msn.com',
-            disabled: true,
-            key: 'key5',
-            target: '_blank',
-          },
-        ],
-        isExpanded: true,
-      
-      
-      },
-    ],
-  },
-];
 
-const NavBasicExample: React.FunctionComponent = () => {
-  return (
-    <Nav
-      onLinkClick={_onLinkClick}
-      selectedKey="key3"
-      ariaLabel="Nav basic example"
-      styles={navStyles}
-      groups={navLinkGroups}
-    />
-  );
-};
-
-function _onLinkClick(ev?: React.MouseEvent<HTMLElement>, item?: INavLink) {
-  if (item && item.name === 'News') {
-    alert('News link clicked');
-  }
 }
 
-const NavBasicExampleWrapper = () => <Fabric><NavBasicExample /></Fabric>;
-ReactDOM.render(<NavBasicExampleWrapper />, document.getElementById('content'))
+export default Navigation;
