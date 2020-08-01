8aa021bc1918d6e78e43bed532df86753ffc9327
Cleveland Pitt <cleveland.pitt@gmail.com>
2020-08-01 16:32:11 -0400

    finished social media component

---
+++ portfolio/src/components/Social.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Social extends React.Component {
+    render() {
+        return (
+            <section className="sg-social ms-textAlignCenter">
+                <a href="javascript:void(0);"><i className="fab fa-twitter" /></a>
+                <a href="javascript:void(0);"><i className="fab fa-instagram" /></a>
+                <a href="javascript:void(0);"><i className="fab fa-linkedin-in" /></a>
+                <a href="javascript:void(0);"><i className="fab fa-github-square" /></a>
+            </section>
+        )
+    }
+}
+
+export default Social;
